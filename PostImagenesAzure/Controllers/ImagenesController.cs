60cd1c35a9ca86284f5c647ea2166a9796e0b96d
andres <andrellu.o.f@gmail.com>
2022-04-30 12:37:07 +0200

    Add project files.

---
+++ PostImagenesAzure/Controllers/ImagenesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostImagenesAzure.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostImagenesAzure.Controllers
+{
+    public class ImagenesController : Controller
+    {
+        private ServiceStorageBlobs service;
+
+        public ImagenesController(ServiceStorageBlobs service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<string> imagenes = await this.service.GetBlobsAsync("pokemon");
+            return View(imagenes);
+        }
+
+        public IActionResult UploadImagen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImagen(String nombreImagen,IFormFile imagenBBDD)
+        {
+            string extension = imagenBBDD.FileName.Split(".")[1];
+            string fileName = nombreImagen.Trim() + "." + extension;
+            using (Stream stream = imagenBBDD.OpenReadStream())
+            {
+                await this.service.UploadBlobAsync("pokemon", fileName, stream);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string nombre)
+        {
+            await this.service.DeleteBlobAsync("pokemon", nombre);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PostImagenesAzure/Services/ServiceStorageBlobs.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostImagenesAzure.Services
+{
+    public class ServiceStorageBlobs
+    {
+        private BlobServiceClient client;
+        protected string Pokemon = "pokemon";
+
+        public ServiceStorageBlobs(BlobServiceClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task DeleteBlobAsync(string containerName, string blobName)
+        {
+            BlobContainerClient folder = this.client.GetBlobContainerClient(containerName);
+            await folder.DeleteBlobAsync(blobName);
+        }
+
+        public async Task<List<string>> GetBlobsAsync(string containerName)
+        {
+            BlobContainerClient folder = this.client.GetBlobContainerClient(containerName);
+            List<string> imagenes = new List<string>();
+            await foreach (BlobItem images in folder.GetBlobsAsync())
+            {
+                imagenes.Add(images.Name);
+            }
+            return imagenes;
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream stream)
+        {
+            BlobContainerClient containerClient = this.client.GetBlobContainerClient(containerName);
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(stream, overwrite: true);
+        }
+    }
+}
